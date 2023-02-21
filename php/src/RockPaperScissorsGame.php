1fc11cada3026ac3cc5b15ccbe85bc31ee41f645
FuenSansano <fuensanosano@gmail.com>
2023-02-22 00:03:42 +0100

    GREEN: given_I_have_chosen_rock_when_the_opponent_chooses_paper_then_the_opponent_should_win

--- php/src/RockPaperScissorsGame.php
+++ php/src/RockPaperScissorsGame.php
@@ -5,10 +5,15 @@ namespace Kata;
 use Kata\Movement;
 use Kata\Player;
 
+
 class RockPaperScissorsGame
 {
     public function play(Movement $player1move, Movement $player2move): Player
     {
+        if($player2move === Movement::PAPER) {
+            return Player::PLAYER_2;
+        }
+
         if ($player1move === Movement::ROCK || $player1move === Movement::PAPER) {
             return Player::PLAYER_1;
         }
