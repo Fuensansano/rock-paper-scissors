e7e716b3bbb6bc8914df2cbf657f0a9c46c79837
FuenSansano <fuensanosano@gmail.com>
2023-02-21 23:25:44 +0100

    GREEN: given_I_have_chosen_paper_when_the_opponent_chooses_rock_then_I_should_win

--- php/src/RockPaperScissorsGame.php
+++ php/src/RockPaperScissorsGame.php
@@ -6,7 +6,7 @@ class RockPaperScissorsGame
 {
     public function play(string $player1move, string $player2move): string
     {
-        if ($player1move === 'rock') {
+        if ($player1move === 'rock' || $player1move === 'paper') {
             return 'player1';
         }
         return 'player2';
